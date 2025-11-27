e80ec93be138f2e53537065cdba6c6b79225ae3c
Jayanthan <jayanthan1944@gmail.com>
2025-11-27 16:46:29 +0530

    fixed sql problems 3

--- backend/src/routes/systemRoutes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { getSystemLogs } from "../controllers/systemController.js";
-const router = Router();
-router.get("/logs", getSystemLogs);
-export default router;
