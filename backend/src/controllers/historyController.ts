48fb8d69723189cc61f07b2f6499a8a3696d7845
Jayanthan <jayanthan1944@gmail.com>
2025-11-27 16:35:37 +0530

    fixed backend routes 2

--- backend/src/controllers/historyController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { History } from "../models/historyModels.js";
-import { AuthedRequest } from "../middleware/auth";
-import { Thread } from "../models/threadModel.js";
-export const getThreads = async (req: AuthedRequest, res: Response) => {
-  const userId = req.auth?.userId;
-  const threads = await Thread.find({ userId }).sort({ updatedAt: -1 });
-  res.json(threads);
-};
