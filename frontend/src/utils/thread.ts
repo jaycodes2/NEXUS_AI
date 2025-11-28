3ed9bd5b929ce3b0167eaadc8a62832ea88b7e64
Jayanthan <jayanthan1944@gmail.com>
2025-11-29 01:35:36 +0530

    fixed thread issue

--- frontend/src/utils/thread.ts
+++ frontend/src/utils/thread.ts
@@ -1,12 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 export function getThreadId() {
-  let id = localStorage.getItem("threadId");
-  if (!id) {
-    id = uuidv4();
-    localStorage.setItem("threadId", id);
-  }
-  return id;
+  return localStorage.getItem("threadId");
 }
 
 export function newThread() {
