576b7866797561da70713672e679cd591a2dc3d7
Jayanthan <jayanthan1944@gmail.com>
2025-11-28 23:54:47 +0530

    edited App 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         {/* App Routes */}
         {appState === "app" && (
           <>
-            <Route path="/docs" element={<Documentation />} />
+            <Route path="/docs" element={<Documentation onBack={() => window.history.back()} />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route
               path="/login"
